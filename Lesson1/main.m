7f291fb96a7b04c5f83fe3bbf5ac1fb0be33b9ca
lolo <lolo@macbook-air-lolo.local>
2016-12-16 20:26:21 +0300

    Initial Commit

---
+++ Lesson1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Lesson1
+//
+//  Created by lolo on 16.12.16.
+//  Copyright © 2016 lolo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
